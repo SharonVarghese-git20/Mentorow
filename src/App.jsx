9f327e9e20fc64d8b6d8ecc50f116e6bea7b35d3
Sharon Varghese <varghesesharon423@gmail.com>
2024-02-29 18:10:52 +0530

    contact-phase1

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import {useState} from 'react';
+import './App.css';
+import './index.css';
+import Contact from './component/ContactPage';
+
+const App=()=>{
+    return(
+        <div>
+            <Contact></Contact>
+        </div>
+    );
+}
+export default App; 
---
+++ src/component/ContactPage.jsx
@@ -0,0 +1,18 @@
+import {useState} from 'react';
+const Contact=()=> {
+
+  return (
+    <div>
+     <div className='ContactS1'>
+      <p><h1>Still Confused About Your Career Choice?Consult With Our Experts</h1>
+      <button>Contact Us</button>
+     </p>
+     <img src={require('./img/img2.jpg')}></img>
+    
+     </div>
+    
+    </div>
+  );
+};
+
+export default Contact;
