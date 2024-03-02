bda55b404bde6e7091c168b5f33615f0f1bb500d
Sharon Varghese <varghesesharon423@gmail.com>
2024-03-02 22:31:33 +0530

    modified  commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,11 @@ import {useState} from 'react';
 import './App.css';
 import './index.css';
 import Contact from './component/ContactPage';
+import React from 'react';
+
+import { useMediaQuery } from 'react-responsive';
+
+import MediaQuery from 'react-responsive';
 
 const App=()=>{
     return(
--- src/component/ContactPage.jsx
+++ src/component/ContactPage.jsx
@@ -1,16 +1,23 @@
 import {useState} from 'react';
+import React from 'react';
+
+import { useMediaQuery } from 'react-responsive';
+
+import MediaQuery from 'react-responsive'
+
 const Contact=()=> {
 
   return (
-    <div>
-     <div className='ContactS1'>
-      <p><h1>Still Confused About Your Career Choice?Consult With Our Experts</h1>
-      <button>Contact Us</button>
-     </p>
-     <img src={require('./img/img2.jpg')}></img>
+    <div >
+     <div className='ContactS1' >
+          <div className="pt-8"><h1 className=" md:tracking-wide" >Still Confused About Your Career Choice?Consult With Our Experts</h1>
+          
+          <button>Contact Us</button>
+          </div>
+     
+     <img className="object-cover h-[610px] w-[600px] rounded-full bg-neutral-950 " src={require('./img/img2.jpg')} ></img>
     
      </div>
-    
     </div>
   );
 };
